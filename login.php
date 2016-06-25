e8dca1eb310cec9e66c901d9ef5c95a5bb5c01ca
Dinushanka Nayomal <Rama41222@Dinushankas-MacBook-Pro.local>
2016-06-25 22:18:32 +0530

    first commit

---
+++ login.php
@@ -0,0 +1,70 @@
+
+<html>
+<head>
+<title>PowerSteels ERP</title>
+<style>
+#bod {
+    border: 1px solid #3498db;
+    border-radius: 5px;
+	Padding-bottom:10px;
+	box-shadow: 1px 1px 10px #95a5a6;
+}
+
+#bb{
+	
+	background-color: #3498db;
+    color: white;
+    border:1px solid #3498db;
+	
+}
+
+.lblshade{
+
+	color: #95a5a6;
+}
+
+
+</style>
+
+</head>
+
+<body> 
+
+<div class = "cantainer row" style=margin-top:10%;"> 
+<div class = "col-md-4"> </div>
+<div id="bod"  class = "col-md-4 wow zoomInLeft" data-wow-delay="0.3s" > 
+
+	<h1 align="center"><small>PowerSteels</small> <br/>ERP </h1> <hr/>
+	<form> 
+		<div class = "form-group">
+		<label class ="lblshade" for="un">Username : </label><br/>	
+		<input id ="un" type = "text" name = "userName" class = "form-control" placeholder="Username"/><br/>
+		<label class ="lblshade" for="psw">Password : </label><br/>	
+		<input id="psw" type = "password" name="pw" class = "form-control" placeholder="Password"><br/>
+		<button id="bb"class = "btn btn-md "  style="margin:auto; display:block; float:center; border-radius:5px; padding-left:15px;padding-right:15px" type="submit" name="login"><i class="fa fa-sign-in fa-lg" aria-hidden="true"></i> Login</button>
+		</div>
+	</Form>
+	
+	
+
+
+
+</div>
+<div class = "col-md-4"> </div>
+</div>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"/>
+<!- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+<link rel="stylesheet" href="css/animate.css">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+ <script src="js/wow.min.js"></script>
+              <script>
+              new WOW().init();
+              </script>
+</body>
+
+
+</html>
