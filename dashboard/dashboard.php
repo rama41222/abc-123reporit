bacb8a161f9349a83663a96bad373cede8154541
Dinushanka Nayomal <Rama41222@Dinushankas-MacBook-Pro.local>
2016-06-26 17:19:52 +0530

    recreated the dash with bootstrap

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -2,35 +2,142 @@
 <html>
 <head> 
 <title>Dashboard</title>
-<link rel="stylesheet"  type="text/css" href="styles/style.css">
+<style>
+	
+	#space {
+		width:250px;
+		border-right:1px solid #3498db ;
+		background-color: #6C7A89;
+	}
 
-<meta name="viewport" conntent="width=device-width,initial-scale:1.0,userscalable=0">
+	#sidebar{
+
+		    position: relative;
+
+		background-color: #6C7A89;
+		float:left;
+	
+		margin-top:0;
+		margin-bottom: 0;
+		width:251px;
+		max-height: 100% !important;
+		min-height:100%	!important;
+		border-right:1px solid #3498db ;
+		border-left:1px solid white;
+		box-shadow: 1px 1px 10px #95a5a6;
+	}
+
+.navbar{
+
+	background-color: white;
+	border-bottom: 1px solid #3498db !important;
+    border-radius: 5px !important;
+	box-shadow: 1px 1px 10px #95a5a6 !important;
+	color:white !important;
+
+
+}
+a{
+
+	text-decoration: none !important;
+	box-sizing:border-box !important;
+
+}
+
+ul#sidenav li{
+	list-style:none!important;
+	
+
+}
+
+
+ul#sidenav li a{
+	color: #ccc;
+	display: block;
+	padding: 15px;
+	font-size: 1.2em;
+		
+	-webkit-transition:0.2s;
+	-moz-transition:0.2s;
+	-ms-transition:0.2s;
+	-o-transition:0.2s;
+	transition:0.2s;
+
+
+}
+ul#sidenav li a.selected {
+	background-color: #ABB7B7;
+	color: #fff;
+	border-radius: 5px;
+
+
+}
+ul#sidenav li a:hover{
+	background-color: #ABB7B7;
+	border: 1px solid #ABB7B7;
+	border-radius: 5px;
+
+	color: #fff;
+	padding-left: 30px;
+}
+
+</style>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
 </head>
 
 <body> 
-<div id="header">
-	<div class="logo"> <a href="#"> PowerSteels <span>ERP </span> </a> </div>
-</div>
-
+<header>
 	
+	<nav class="navbar navbar-default" style="margin-bottom:0;">
+  <div class="container-fluid">
+    <div class="navbar-header">
 
-<div id= "container"> 
-	<div class="sidebar"> 
-		<ul id="nav">
-			<li> <a class="selected" href="#"> Dashboard </a> </li>
-			<li> <a href="#"> Dashboard </a> </li>
-			<li> <a href="#"> Dashboard </a> </li>
-			<li> <a href="#"> Dashboard </a> </li>
-			<li> <a href="#"> Dashboard </a> </li>
-			<li> <a href="#"> Dashboard </a> </li>
-			<li> <a href="#"> Dashboard </a> </li>
-			<li> <a href="#"> Dashboard </a> </li>
-		</ul>
-	</div>
-	<div class="content"> 
-			
-	</div>
+     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+
+
+      <a id="space" class="navbar-brand" href="#">
+        <i class="fa fa-paper-plane" aria-hidden="true"></i> PowerSteels ERP
+      </a>
+    </div>
+     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+     	<form class="navbar-form navbar-left" role="search">
+  <div class="form-group">
+    <input type="text" class="form-control" placeholder="Search">
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+
+
+     </div>
+
+  </div>
+</nav>
+</header>
+<div class = "container row">
+<div id="sidebar" class="container-fluid col-md-3 ">
+	<ul id="sidenav" style="padding-left:0px;">
+		<li><a href="#"><i class="fa fa-pencil-square fa-2x" aria-hidden="true"></i> Dashboard</a></li>
+		<li><a href="#"><i class="fa fa-pencil-square fa-2x" aria-hidden="true"></i> Dashboard</a></li>
+		<li><a href="#"><i class="fa fa-pencil-square fa-2x" aria-hidden="true"></i> Dashboard</a></li>
+		<li><a href="#"><i class="fa fa-pencil-square fa-2x" aria-hidden="true"></i> Dashboard</a></li>
+		<li><a href="#"><i class="fa fa-pencil-square fa-2x" aria-hidden="true"></i> Dashboard</a></li>
+		<li><a href="#"><i class="fa fa-pencil-square fa-2x" aria-hidden="true"></i> Dashboard</a></li>
+		<li><a href="#"><i class="fa fa-pencil-square fa-2x" aria-hidden="true"></i> Dashboard</a></li>
+		<li><a href="#"><i class="fa fa-pencil-square fa-2x" aria-hidden="true"></i> Dashboard</a></li>
+	</ul>
 </div>
+<div class="col-md-9"></div>
+3wrew3r
+
+</div>	
 
 
 
@@ -38,6 +145,10 @@
 
 
 
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"/>
+<script   src="https://code.jquery.com/jquery-3.0.0.min.js"   integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0="   crossorigin="anonymous"></script>
+<script   src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
