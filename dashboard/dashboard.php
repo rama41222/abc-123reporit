1559a88c8937de3225f339e1c9c1b0e3ecbbbafd
Alupotha <un.alupotha1994@gmail.com>
2016-06-26 13:48:10 +0530

    created the dashboard

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1,3 +1,52 @@
+
 <html>
+<head> 
+<title>Dashboard</title>
+<link rel="stylesheet"  type="text/css" href="styles/style.css">
+
+<meta name="viewport" conntent="width=device-width,initial-scale:1.0,userscalable=0">
+</head>
+
+<body> 
+<div id="header">
+	<div class="logo"> <a href="#"> PowerSteels <span>ERP </span> </a> </div>
+</div>
+
+	
+
+<div id= "container"> 
+	<div class="sidebar"> 
+		<ul id="nav">
+			<li> <a class="selected" href="#"> Dashboard </a> </li>
+			<li> <a href="#"> Dashboard </a> </li>
+			<li> <a href="#"> Dashboard </a> </li>
+			<li> <a href="#"> Dashboard </a> </li>
+			<li> <a href="#"> Dashboard </a> </li>
+			<li> <a href="#"> Dashboard </a> </li>
+			<li> <a href="#"> Dashboard </a> </li>
+			<li> <a href="#"> Dashboard </a> </li>
+		</ul>
+	</div>
+	<div class="content"> 
+			
+	</div>
+</div>
+
+
+
+
+
+
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+
 	
 </html>
