ed539d8c09bc134a2b795834a3627337a0ddeab9
Dinushanka Nayomal <Rama41222@Dinushankas-MacBook-Pro.local>
2016-06-26 11:18:32 +0530

    few asthetic changes to button and added a dashboard

--- login.php
+++ login.php
@@ -16,7 +16,7 @@
 			<input id ="un" type = "text" name = "userName" class = "form-control" placeholder="Username"/><br/>
 			<label class ="lblshade" for="pp">Password : </label><br/>	
 			<input id="pp" type = "password" name="pw" class = "form-control" placeholder="Password"><br/>
-			<button id="bb"class = "btn btn-md "  style="margin:auto; display:block; float:center; border-radius:5px; padding-left:15px;padding-right:15px" type="submit" name="login"><i class="fa fa-sign-in fa-lg" aria-hidden="true"></i> Login</button>
+			<button id="bb" class = "btn btn-md" style="margin:auto; display:block; float:center; border-radius:5px; padding-left:15px;padding-right:15px" type="submit" name="login"><i class="fa fa-sign-in fa-lg" aria-hidden="true"></i> Login</button>
 			</div>
 		</form>
 		
@@ -28,6 +28,7 @@
 	<div class ="col-md-4"> </div>
 </div>
 <!-- Latest compiled and minified CSS -->
+
 <link rel="stylesheet" href="css/style.css" type="text/css"/>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"/>
@@ -35,10 +36,20 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 <link rel="stylesheet" href="css/animate.css">
 <!-- Latest compiled and minified JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
  <script src="js/wow.min.js"></script>
               <script>
-              new WOW().init();
+               wow = new WOW(
+                      {
+                      boxClass:     'wow',      // default
+                      animateClass: 'animated', // default
+                      offset:       0,          // default
+                      mobile:       true,       // default
+                      live:         true        // default
+                    }
+                    )
+                    wow.init();
               </script>
 </body>
 
